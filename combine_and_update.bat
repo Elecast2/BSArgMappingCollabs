dc9f16e0363b040e8a26bd6c325ab827b843ea57
Elecast <diegocastellano08@gmail.com>
2024-03-07 17:13:37 -0300

    Agregados .bat para simplificar el workflow

---
+++ combine_and_update.bat
@@ -0,0 +1,29 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: Lista las carpetas y pide al usuario que elija una
+echo Selecciona una carpeta:
+set /a count=0
+for /d %%d in (*) do (
+    set /a count+=1
+    set "folder!count!=%%d"
+    echo !count!. %%d
+)
+
+:: Recibe la selección del usuario
+set /p choice="Ingresa el numero de la carpeta: "
+set "selectedFolder=!folder%choice%!"
+
+:: Verifica si se ha seleccionado una carpeta
+if defined selectedFolder (
+    echo Has seleccionado la carpeta: !selectedFolder!
+    git pull origin main
+    node merge.js "!selectedFolder!"
+    set /p commitMessage="Ingrese el mensaje con los cambios: "
+    git add .
+    git commit -m "!commitMessage!"
+    git push origin main
+) else (
+    echo Selección inválida.
+)
+pause
